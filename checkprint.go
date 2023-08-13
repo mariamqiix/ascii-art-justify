ca2eacad80325c3b3bdf75d98d8d1cac280b07db
maabbas <mx38x@iclcoud.com>
2023-08-13 17:35:14 +0300

    idk

--- checkprint.go
+++ checkprint.go
@@ -1,19 +1,19 @@
 package ascii
 
-// import "fmt"
+import "fmt"
 
 func CheckPrint(s [][]string, justify string, count2 int) int {
 	j := ""
 	for n := 0; n < len(s); n++ {
-		if justify == "justify" {
-			if s[n][4] != "      " {
-				j += s[n][4]
-			} else if s[n][4] == "      " && count2 < 4 {
-				j += s[n][4]
-			}
-		} else {
+		// if justify == "justify" {
+		// 	if s[n][4] != "      " {
+		// 		j += s[n][4]
+		// 	} else if s[n][4] == "      " && count2 < 4 {
+		// 		j += s[n][4]
+		// 	}
+		// } else {
 			j += s[n][4]
-		}
+
 	}
 	width := width()
 	if len(j) <= width {
@@ -24,18 +24,24 @@ func CheckPrint(s [][]string, justify string, count2 int) int {
 		if justify != "justify" {
 			return z
 		}
+		if z*count2+len(j) != width-4 {
+		if z*count2+len(j) > width-4 {
+			z = (z*count2)-((z*count2+len(j)-width))
+			// fmt.Print(z)
 
-		if z*count2 > width {
-			if count2 > 4 {
-				if z > width/(count2*4) {
-					z -= count2*4 + 3
-				}
-			} else {
-				z -= count2*3 + 3
-			}
-
+			return  z
+		} else if z*count2+len(j) < width {
+			z = (z*count2)-((z*count2+len(j)-width))
+			z += 4
+			fmt.Print(z)
+			return  z
 		}
-		return z
-	}
+			} else {
+				return z+4
+			}
+return z
+		}
+		
+	
 	return 1
 }
--- printwithjustify.go
+++ printwithjustify.go
@@ -11,11 +11,11 @@ func PrintWithJustify(Words [][]string, justify string, n, w, count int) string 
 		return fmt.Sprintf("%"+strconv.Itoa(CheckPrint(Words, justify, count))+"v", Words[n][w])
 	} else if justify == "center" && n == 0 {
 		return fmt.Sprintf("%"+strconv.Itoa(CheckPrint(Words, justify, count)/2+3)+"v", Words[n][w])
-	} else if justify == "justify" && n != 0 && Words[n-1][w] == "      " {
+	} else if justify == "justify" && n != 0 && Words[n][w] == "      " {
 		if count == 0 {
 			count = 1
 		}
-		return fmt.Sprintf("%"+strconv.Itoa((CheckPrint(Words, justify, count)/count)+3)+"v", Words[n][w])
+		return fmt.Sprintf("%"+strconv.Itoa((CheckPrint(Words, justify, count)/count)+6)+"v", Words[n][w])
 	} else {
 		return fmt.Sprintf("%"+strconv.Itoa(align)+"v", Words[n][w])
 
