b34c1b1358b8c9c7ad2b22541c6cdac18e418103
Mariam Abbas <maabbas@noreply.learn.reboot01.com>
2023-08-13 17:02:57 +0000

    Update 'printwithjustify.go'

--- printwithjustify.go
+++ printwithjustify.go
@@ -2,22 +2,25 @@ package ascii
 
 import (
 	"fmt"
-	"strconv"
+	"strings"
 )
 
 func PrintWithJustify(Words [][]string, justify string, n, w, count int) string {
 	align := 0
 	if justify == "right" && n == 0 {
-		return fmt.Sprintf("%"+strconv.Itoa(CheckPrint(Words, justify, count))+"v", Words[n][w])
+		align = CheckPrint(Words, justify, count)
+		return fmt.Sprintf(strings.Repeat(" ",align) + Words[n][w])
 	} else if justify == "center" && n == 0 {
-		return fmt.Sprintf("%"+strconv.Itoa(CheckPrint(Words, justify, count)/2+3)+"v", Words[n][w])
-	} else if justify == "justify" && n != 0 && Words[n][w] == "      " {
+		align = CheckPrint(Words, justify, count)/2
+		return fmt.Sprintf(strings.Repeat(" ",align) + Words[n][w])
+	} else if justify == "justify" && n != 0 && Words[n][3] == "      " &&  Words[n][5] == "      " && Words[n][7] == "      " {
 		if count == 0 {
 			count = 1
 		}
-		return fmt.Sprintf("%"+strconv.Itoa((CheckPrint(Words, justify, count)/count)+6)+"v", Words[n][w])
+		align = CheckPrint(Words, justify, count)/count
+		return fmt.Sprintf(strings.Repeat(" ",align)+Words[n][w])
 	} else {
-		return fmt.Sprintf("%"+strconv.Itoa(align)+"v", Words[n][w])
+		return fmt.Sprintf( Words[n][w])
 
 	}
 	return ""
