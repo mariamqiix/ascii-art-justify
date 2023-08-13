1f0a64f3d09f6707a92aecb8bd71cfb706d73ac9
Mariam Abbas <maabbas@noreply.learn.reboot01.com>
2023-08-13 17:03:28 +0000

    Update 'checkprint.go'

--- checkprint.go
+++ checkprint.go
@@ -5,14 +5,7 @@ import "fmt"
 func CheckPrint(s [][]string, justify string, count2 int) int {
 	j := ""
 	for n := 0; n < len(s); n++ {
-		// if justify == "justify" {
-		// 	if s[n][4] != "      " {
-		// 		j += s[n][4]
-		// 	} else if s[n][4] == "      " && count2 < 4 {
-		// 		j += s[n][4]
-		// 	}
-		// } else {
-			j += s[n][4]
+			j += s[n][3]
 
 	}
 	width := width()
@@ -20,24 +13,22 @@ func CheckPrint(s [][]string, justify string, count2 int) int {
 		if len(j) == width {
 			return 1
 		}
-		z := width - len(j) + 4
+		z := width - len(j)
 		if justify != "justify" {
 			return z
 		}
-		if z*count2+len(j) != width-4 {
-		if z*count2+len(j) > width-4 {
+		if z*count2+len(j) != width{
+		if z*count2+len(j) > width{
 			z = (z*count2)-((z*count2+len(j)-width))
-			// fmt.Print(z)
 
 			return  z
 		} else if z*count2+len(j) < width {
 			z = (z*count2)-((z*count2+len(j)-width))
-			z += 4
 			fmt.Print(z)
 			return  z
 		}
 			} else {
-				return z+4
+				return z
 			}
 return z
 		}
